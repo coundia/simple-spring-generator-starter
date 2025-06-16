b9dd91cfede49cb021e2e816e88b1227020744e2
pcoundia <papacoundia@gmail.com>
2025-06-16 17:13:18 +0000

    [wip]

---
+++ src/main/java/com/pcoundia/simple_spring_generator_starter/SimpleSpringGeneratorStarterApplication.java
@@ -0,0 +1,13 @@
+package com.pcoundia.simple_spring_generator_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringGeneratorStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringGeneratorStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcoundia/simple_spring_generator_starter/SimpleSpringGeneratorStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcoundia.simple_spring_generator_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringGeneratorStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
